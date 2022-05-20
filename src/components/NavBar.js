63fe94b9011198ae49342f7fd31b46c153ed9c9f
mezitta <michaelezitta@gmail.com>
2022-05-20 12:01:50 -0400

    add bootstrap and format nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <div>
+        <Container fluid className='bar'>
             <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/projects">Projects</NavLink>
-                <NavLink to="/resume">Resume</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
+                <Row className="nav-row">
+                    <Col className="nav-col">
+                        <NavLink to="/">Home</NavLink>
+                    </Col>
+                    <Col className="nav-col">
+                        <NavLink to="/projects">Projects</NavLink>
+                    </Col>
+                    <Col className="nav-col">
+                        <NavLink to="/resume">Resume</NavLink>
+                    </Col>
+                    <Col className="nav-col">
+                        <NavLink to="/contact">Contact</NavLink>
+                    </Col>
+                </Row>
             </nav>
-        </div>
+        </Container>
     )
 }
 
