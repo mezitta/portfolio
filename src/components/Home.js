6d5204bcad462d31194fe4fdf6f0d4187c0a9d0d
mezitta <michaelezitta@gmail.com>
2022-05-26 12:51:26 -0400

    Home page styling changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,16 +16,26 @@ const Home = () => {
         <div className="home-body">
            <Container fluid>
                <Row className="home-row">
-                    <Col sm={6} className="home-left">
-                        Howdy! I'm Michael Zitta<br/>
-                        Front End Web Developer<br/>
-                        I am passionate about creating responsive web applications<br/>
-                        <NavLink to="/projects"><button>Check Out My Work!</button></NavLink>
-                        <div>
-                            <a target="_blank" rel="noreferrer" href="https://github.com/mezitta"><FontAwesomeIcon icon={faGithub} /></a>
-                            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-zitta/"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <Col sm={12} lg={6} className="home-left">
+                        <div className="home-text">
+                            <div>Howdy! I'm Michael Zitta</div>
+                            <div className="role">Front End Web Developer</div>
+                            <div>I am passionate about creating responsive web applications</div>
                         </div>
+
+                        <Row className="button-row">
+                            <Col>
+                                <NavLink to="/projects"><button className="project-button">Portfolio</button></NavLink>
+                                <NavLink to="/contact"><button className="project-button">Hire me</button></NavLink>
+                            </Col>
+                        </Row>
+                    
+                        <Row className="social-links">
+                            <Col xs={1}><a target="_blank" rel="noreferrer" href="https://github.com/mezitta"><FontAwesomeIcon icon={faGithub} /></a></Col>
+                            <Col xs={1}><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-zitta/"><FontAwesomeIcon icon={faLinkedin} /></a></Col>
+                        </Row>
                     </Col>
+
                     <Col sm={6} className="home-right">
                         Howdy<br/>
                         Howdy<br/>
